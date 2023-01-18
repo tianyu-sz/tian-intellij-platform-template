0710ebbff5bb403adf0fec263efb07262fd853a1
GitHub Action <action@github.com>
2023-01-18 02:55:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/tianyusz/tianintellijplatformtemplate/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/tianyusz/tianintellijplatformtemplate/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.tianyusz.tianintellijplatformtemplate.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.tianyusz.tianintellijplatformtemplate.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/tianyusz/tianintellijplatformtemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/tianyusz/tianintellijplatformtemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.tianyusz.tianintellijplatformtemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tianyusz.tianintellijplatformtemplate.MyBundle
 
 class MyApplicationService {
 
